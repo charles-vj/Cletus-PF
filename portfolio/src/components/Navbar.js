bd4f5ecd74dc83ea4fa27e8eddca75f6927a2f8d
charles-vj <charles2019@iiitkottayam.ac.in>
2021-02-18 03:20:10 +0530

    skeleton

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,16 +1,73 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import {Link } from 'react-router-dom'
+import { MenuData } from '../data/MenuData';
+import { Button } from './Button';
 
 
 const Nav = styled.nav`
-    height : 500px;
+    height : 60px;
     background :red;
-`
+    display : flex;
+    justify-content :  space-between;
+    padding : 1rem 2rem;
+    z-index : 100;
+    position : fixed;
+    width : 100%
+    
+`;
+
+const Navlink = css`
+   color : #fff;
+   display : flex;
+   align-items : center;
+   padding : 0 1rem;
+   height : 100%;
+   cursor : pointer;
+   text-decoration : none;
+`;
+
+const Logo = styled(Link)`
+    ${Navlink}
+    font-style : italic;
+`;
+
+const MenuBar = styled.i``;
+
+const Navmenu = styled.i`
+    display : flex;
+    align-items : center;
+    margin-right : -48px;
+    font : normal;
+`;
+
+const NavMenuLinks = styled(Link)`
+    ${Navlink}
+    font : normal;
+`;
+
+const NavBtn = styled.div`
+    display : flex;
+    align-items : center;
+    margin-right : 24px;
+`;
 
 const Navbar = () => {
     return (
         <Nav>
-            <h1>i am navbar</h1>
+            <Logo to="/">Cletus</Logo>
+            <MenuBar />
+            <Navmenu>
+                {MenuData.map((item,index) => (
+                    <NavMenuLinks to = {item.link}  key={index}>
+                        {item.title}
+                    </NavMenuLinks>
+                ))}
+            </Navmenu>
+            <NavBtn>
+                <Button to="./contact" primary='true'>Contact us</Button>
+            </NavBtn>
+            
         </Nav>
     )
 }
---
+++ portfolio/src/data/MenuData.js
@@ -0,0 +1,5 @@
+export const MenuData = [
+    { title : 'About', link : '/about'},
+    { title : 'Work', link : '/work'},
+    { title : 'Achievments', link : '/achievments'}
+]
